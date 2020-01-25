77ed2e7794cda6a30ab5e1a6c22ca8eb2e6b9f23
Hristo Oskov <hristo@openlattice.com>
2020-01-24 18:48:12 -0800

    comment out submit logic for now

--- src/containers/consent/ConsentContainer.js
+++ src/containers/consent/ConsentContainer.js
@@ -222,187 +222,187 @@ class ConsentContainer extends Component<Props, State> {
     }
   }
 
-  onSubmit = () => {
-
-    const {
-      actions,
-      clientEntityKeyId,
-      entitySetIds,
-      geo,
-      propertyTypeIds,
-      staffEntityKeyId,
-    } = this.props;
-    const { data } = this.state;
-    const { geolocation } = geo;
-
-    const additionalWitnessesCount :number = this.getAdditionalWitnessesCount(data);
-    const nowAsISO :string = (new Date()).toISOString();
-
-    const associations = [];
-
-    /*
-     *
-     * form -> includes -> content
-     *
-     */
-
-    // form -> includes -> content
-    associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 0, CONSENT_FORM_CONTENT_ESN, {
-      [OL_DATE_TIME_FQN]: [nowAsISO],
-    }]);
-
-    /*
-     *
-     * form -> includes -> signature
-     *
-     */
-
-    // form -> includes -> signature (client)
-    associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 0, ELECTRONIC_SIGNATURE_ESN, {
-      [OL_DATE_TIME_FQN]: [nowAsISO],
-    }]);
-    // form -> includes -> signature (staff)
-    if (this.isStaffSignatureRequired()) {
-      associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 1, ELECTRONIC_SIGNATURE_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-      }]);
-    }
-    // form -> includes -> signature (witnesses)
-    if (this.areWitnessSignaturesRequired()) {
-      for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
-        // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
-        // have to start at index 2
-        const witnessSignatureIndex :number = witnessIndex + 2;
-        associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, {
-          [OL_DATE_TIME_FQN]: [nowAsISO],
-        }]);
-      }
-    }
-
-    /*
-     *
-     * signature -> located at -> location
-     *
-     */
-
-    // signature (client) -> located at -> location
-    associations.push([LOCATED_AT_ESN, 0, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
-      [OL_DATE_TIME_FQN]: [nowAsISO],
-    }]);
-    // signature (staff) -> located at -> location
-    if (this.isStaffSignatureRequired()) {
-      associations.push([LOCATED_AT_ESN, 1, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-      }]);
-    }
-    // signature (witness) -> located at -> location
-    if (this.areWitnessSignaturesRequired()) {
-      for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
-        // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
-        // have to start at index 2
-        const witnessSignatureIndex :number = witnessIndex + 2;
-        associations.push([LOCATED_AT_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
-          [OL_DATE_TIME_FQN]: [nowAsISO],
-        }]);
-      }
-    }
-
-    /*
-     *
-     * form -> signed by -> person
-     *
-     */
-
-    // form -> signed by -> person (client)
-    associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, clientEntityKeyId, CLIENTS_ESN, {
-      [OL_DATE_TIME_FQN]: [nowAsISO],
-      [OL_ROLE_FQN]: [SigningRoles.CLIENT],
-    }]);
-    // form -> signed by -> person (staff)
-    if (this.isStaffSignatureRequired()) {
-      associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, staffEntityKeyId, STAFF_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-        [OL_ROLE_FQN]: [SigningRoles.STAFF],
-      }]);
-    }
-    // form -> signed by -> person (witness)
-    for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
-      // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
-      // have to start at index 2
-      associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, witnessIndex, WITNESSES_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-        [OL_ROLE_FQN]: [SigningRoles.WITNESS],
-      }]);
-    }
-
-    /*
-     *
-     * signature -> signed by -> person
-     *
-     */
-
-    // signature -> signed by -> person (client)
-    associations.push([SIGNED_BY_ESN, 0, ELECTRONIC_SIGNATURE_ESN, clientEntityKeyId, CLIENTS_ESN, {
-      [OL_DATE_TIME_FQN]: [nowAsISO],
-      [OL_ROLE_FQN]: [SigningRoles.CLIENT],
-    }]);
-    // signature -> signed by -> person (staff)
-    if (this.isStaffSignatureRequired()) {
-      associations.push([SIGNED_BY_ESN, 1, ELECTRONIC_SIGNATURE_ESN, staffEntityKeyId, STAFF_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-        [OL_ROLE_FQN]: [SigningRoles.STAFF],
-      }]);
-    }
-    // signature -> signed by -> person (witness)
-    for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
-      // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
-      // have to start at index 2
-      const witnessSignatureIndex :number = witnessIndex + 2;
-      associations.push([SIGNED_BY_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, witnessIndex, WITNESSES_ESN, {
-        [OL_DATE_TIME_FQN]: [nowAsISO],
-        [OL_ROLE_FQN]: [SigningRoles.WITNESS],
-      }]);
-    }
-
-    const entityMappers = Map().withMutations((mappers :Map) => {
-
-      const keyMappers = Map().withMutations((map :Map) => {
-        map.set(getEntityAddressKey(0, CONSENT_FORM_CONTENT_ESN, OL_TEXT_FQN), JSON.stringify);
-      });
-      mappers.set(KEY_MAPPERS, keyMappers);
-
-      if (this.areWitnessSignaturesRequired()) {
-        const indexMappers = Map().withMutations((map :Map) => {
-          // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
-          // have to start at index 2
-          map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_NAME_FQN), (i) => i + 2);
-          map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), (i) => i + 2);
-          map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), (i) => i + 2);
-        });
-        mappers.set(INDEX_MAPPERS, indexMappers);
-      }
-
-      const valueMappers = Map().withMutations((map :Map) => {
-        map.set(getEntityAddressKey(0, LOCATION_ESN, LOCATION_LATITUDE_FQN), () => geolocation.coords.latitude);
-        map.set(getEntityAddressKey(0, LOCATION_ESN, LOCATION_LONGITUDE_FQN), () => geolocation.coords.longitude);
-        map.set(getEntityAddressKey(0, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
-        map.set(getEntityAddressKey(0, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
-        if (this.isStaffSignatureRequired()) {
-          map.set(getEntityAddressKey(1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
-          map.set(getEntityAddressKey(1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
-        }
-        if (this.areWitnessSignaturesRequired()) {
-          map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
-          map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
-        }
-      });
-      mappers.set(VALUE_MAPPERS, valueMappers);
-    });
-
-    const entityData = processEntityData(data, entitySetIds, propertyTypeIds, entityMappers);
-    const associationEntityData = processAssociationEntityData(associations, entitySetIds, propertyTypeIds);
-    actions.submitConsent({ associationEntityData, entityData, timestamp: nowAsISO });
-  }
+  // onSubmit = () => {
+  //
+  //   const {
+  //     actions,
+  //     clientEntityKeyId,
+  //     entitySetIds,
+  //     geo,
+  //     propertyTypeIds,
+  //     staffEntityKeyId,
+  //   } = this.props;
+  //   const { data } = this.state;
+  //   const { geolocation } = geo;
+  //
+  //   const additionalWitnessesCount :number = this.getAdditionalWitnessesCount(data);
+  //   const nowAsISO :string = (new Date()).toISOString();
+  //
+  //   const associations = [];
+  //
+  //   /*
+  //    *
+  //    * form -> includes -> content
+  //    *
+  //    */
+  //
+  //   // form -> includes -> content
+  //   associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 0, CONSENT_FORM_CONTENT_ESN, {
+  //     [OL_DATE_TIME_FQN]: [nowAsISO],
+  //   }]);
+  //
+  //   /*
+  //    *
+  //    * form -> includes -> signature
+  //    *
+  //    */
+  //
+  //   // form -> includes -> signature (client)
+  //   associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 0, ELECTRONIC_SIGNATURE_ESN, {
+  //     [OL_DATE_TIME_FQN]: [nowAsISO],
+  //   }]);
+  //   // form -> includes -> signature (staff)
+  //   if (this.isStaffSignatureRequired()) {
+  //     associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, 1, ELECTRONIC_SIGNATURE_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //     }]);
+  //   }
+  //   // form -> includes -> signature (witnesses)
+  //   if (this.areWitnessSignaturesRequired()) {
+  //     for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
+  //       // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
+  //       // have to start at index 2
+  //       const witnessSignatureIndex :number = witnessIndex + 2;
+  //       associations.push([INCLUDES_ESN, 0, CONSENT_FORM_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, {
+  //         [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       }]);
+  //     }
+  //   }
+  //
+  //   /*
+  //    *
+  //    * signature -> located at -> location
+  //    *
+  //    */
+  //
+  //   // signature (client) -> located at -> location
+  //   associations.push([LOCATED_AT_ESN, 0, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
+  //     [OL_DATE_TIME_FQN]: [nowAsISO],
+  //   }]);
+  //   // signature (staff) -> located at -> location
+  //   if (this.isStaffSignatureRequired()) {
+  //     associations.push([LOCATED_AT_ESN, 1, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //     }]);
+  //   }
+  //   // signature (witness) -> located at -> location
+  //   if (this.areWitnessSignaturesRequired()) {
+  //     for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
+  //       // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
+  //       // have to start at index 2
+  //       const witnessSignatureIndex :number = witnessIndex + 2;
+  //       associations.push([LOCATED_AT_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, 0, LOCATION_ESN, {
+  //         [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       }]);
+  //     }
+  //   }
+  //
+  //   /*
+  //    *
+  //    * form -> signed by -> person
+  //    *
+  //    */
+  //
+  //   // form -> signed by -> person (client)
+  //   associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, clientEntityKeyId, CLIENTS_ESN, {
+  //     [OL_DATE_TIME_FQN]: [nowAsISO],
+  //     [OL_ROLE_FQN]: [SigningRoles.CLIENT],
+  //   }]);
+  //   // form -> signed by -> person (staff)
+  //   if (this.isStaffSignatureRequired()) {
+  //     associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, staffEntityKeyId, STAFF_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       [OL_ROLE_FQN]: [SigningRoles.STAFF],
+  //     }]);
+  //   }
+  //   // form -> signed by -> person (witness)
+  //   for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
+  //     // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
+  //     // have to start at index 2
+  //     associations.push([SIGNED_BY_ESN, 0, CONSENT_FORM_ESN, witnessIndex, WITNESSES_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       [OL_ROLE_FQN]: [SigningRoles.WITNESS],
+  //     }]);
+  //   }
+  //
+  //   /*
+  //    *
+  //    * signature -> signed by -> person
+  //    *
+  //    */
+  //
+  //   // signature -> signed by -> person (client)
+  //   associations.push([SIGNED_BY_ESN, 0, ELECTRONIC_SIGNATURE_ESN, clientEntityKeyId, CLIENTS_ESN, {
+  //     [OL_DATE_TIME_FQN]: [nowAsISO],
+  //     [OL_ROLE_FQN]: [SigningRoles.CLIENT],
+  //   }]);
+  //   // signature -> signed by -> person (staff)
+  //   if (this.isStaffSignatureRequired()) {
+  //     associations.push([SIGNED_BY_ESN, 1, ELECTRONIC_SIGNATURE_ESN, staffEntityKeyId, STAFF_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       [OL_ROLE_FQN]: [SigningRoles.STAFF],
+  //     }]);
+  //   }
+  //   // signature -> signed by -> person (witness)
+  //   for (let witnessIndex = 0; witnessIndex < additionalWitnessesCount; witnessIndex += 1) {
+  //     // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
+  //     // have to start at index 2
+  //     const witnessSignatureIndex :number = witnessIndex + 2;
+  //     associations.push([SIGNED_BY_ESN, witnessSignatureIndex, ELECTRONIC_SIGNATURE_ESN, witnessIndex, WITNESSES_ESN, {
+  //       [OL_DATE_TIME_FQN]: [nowAsISO],
+  //       [OL_ROLE_FQN]: [SigningRoles.WITNESS],
+  //     }]);
+  //   }
+  //
+  //   const entityMappers = Map().withMutations((mappers :Map) => {
+  //
+  //     const keyMappers = Map().withMutations((map :Map) => {
+  //       map.set(getEntityAddressKey(0, CONSENT_FORM_CONTENT_ESN, OL_TEXT_FQN), JSON.stringify);
+  //     });
+  //     mappers.set(KEY_MAPPERS, keyMappers);
+  //
+  //     if (this.areWitnessSignaturesRequired()) {
+  //       const indexMappers = Map().withMutations((map :Map) => {
+  //         // NOTE: the client signature gets index 0 and the staff signature gets index 1, so the witness signatures
+  //         // have to start at index 2
+  //         map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_NAME_FQN), (i) => i + 2);
+  //         map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), (i) => i + 2);
+  //         map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), (i) => i + 2);
+  //       });
+  //       mappers.set(INDEX_MAPPERS, indexMappers);
+  //     }
+  //
+  //     const valueMappers = Map().withMutations((map :Map) => {
+  //       map.set(getEntityAddressKey(0, LOCATION_ESN, LOCATION_LATITUDE_FQN), () => geolocation.coords.latitude);
+  //       map.set(getEntityAddressKey(0, LOCATION_ESN, LOCATION_LONGITUDE_FQN), () => geolocation.coords.longitude);
+  //       map.set(getEntityAddressKey(0, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
+  //       map.set(getEntityAddressKey(0, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
+  //       if (this.isStaffSignatureRequired()) {
+  //         map.set(getEntityAddressKey(1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
+  //         map.set(getEntityAddressKey(1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
+  //       }
+  //       if (this.areWitnessSignaturesRequired()) {
+  //         map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_DATE_TIME_FQN), () => nowAsISO);
+  //         map.set(getEntityAddressKey(-1, ELECTRONIC_SIGNATURE_ESN, OL_SIGNATURE_DATA_FQN), this.signatureValueMapper);
+  //       }
+  //     });
+  //     mappers.set(VALUE_MAPPERS, valueMappers);
+  //   });
+  //
+  //   const entityData = processEntityData(data, entitySetIds, propertyTypeIds, entityMappers);
+  //   const associationEntityData = processAssociationEntityData(associations, entitySetIds, propertyTypeIds);
+  //   actions.submitConsent({ associationEntityData, entityData, timestamp: nowAsISO });
+  // }
 
   // NOTE: this is a temporary fix for getting binary submissions to work. we should figure out a way to not have to
   // hardcode the Content-Type here
@@ -464,7 +464,6 @@ class ConsentContainer extends Component<Props, State> {
           <Form
               formData={data.toJS()}
               onChange={this.onChange}
-              onSubmit={this.onSubmit}
               schema={schema.getIn(['dataSchema', 0])}
               uiSchema={schema.getIn(['uiSchema', 0])} />
         </Card>
