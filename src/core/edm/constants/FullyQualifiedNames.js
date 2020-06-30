e4887b62726ccf74d18488a0b03f293405b28322
Hristo Oskov <hristo@openlattice.com>
2020-06-29 20:24:49 -0700

    bump dependency versions

--- src/core/edm/constants/FullyQualifiedNames.js
+++ src/core/edm/constants/FullyQualifiedNames.js
@@ -4,29 +4,29 @@
 
 import { Models } from 'lattice';
 
-const { FullyQualifiedName } = Models;
+const { FQN } = Models;
 
 const ASSOCIATION_ENTITY_TYPE_FQNS = {};
 const ENTITY_TYPE_FQNS = {};
 
 const PROPERTY_TYPE_FQNS = {
-  GEN_FULL_NAME_FQN: new FullyQualifiedName('general.fullname'),
-  LOCATION_LATITUDE_FQN: new FullyQualifiedName('location.latitude'),
-  LOCATION_LONGITUDE_FQN: new FullyQualifiedName('location.longitude'),
-  OL_ALGORITHM_NAME_FQN: new FullyQualifiedName('ol.algorithmname'),
-  OL_CRYPTO_KEY_FQN: new FullyQualifiedName('ol.cryptokey'),
-  OL_DATE_TIME_FQN: new FullyQualifiedName('ol.datetime'),
-  OL_DESCRIPTION_FQN: new FullyQualifiedName('ol.description'),
-  OL_ELLIPTIC_CURVE_NAME_FQN: new FullyQualifiedName('ol.ellipticcurvename'),
-  OL_HASH_FUNCTION_NAME_FQN: new FullyQualifiedName('ol.hashfunctionname'),
-  OL_ID_FQN: new FullyQualifiedName('ol.id'),
-  OL_NAME_FQN: new FullyQualifiedName('ol.name'),
-  OL_ROLE_FQN: new FullyQualifiedName('ol.role'),
-  OL_SCHEMA_FQN: new FullyQualifiedName('ol.schema'),
-  OL_SIGNATURE_DATA_FQN: new FullyQualifiedName('ol.signaturedata'),
-  OL_TEXT_FQN: new FullyQualifiedName('ol.text'),
-  OL_TYPE_FQN: new FullyQualifiedName('ol.type'),
-  OL_VERSION_FQN: new FullyQualifiedName('ol.version'),
+  GEN_FULL_NAME_FQN: FQN.of('general.fullname'),
+  LOCATION_LATITUDE_FQN: FQN.of('location.latitude'),
+  LOCATION_LONGITUDE_FQN: FQN.of('location.longitude'),
+  OL_ALGORITHM_NAME_FQN: FQN.of('ol.algorithmname'),
+  OL_CRYPTO_KEY_FQN: FQN.of('ol.cryptokey'),
+  OL_DATE_TIME_FQN: FQN.of('ol.datetime'),
+  OL_DESCRIPTION_FQN: FQN.of('ol.description'),
+  OL_ELLIPTIC_CURVE_NAME_FQN: FQN.of('ol.ellipticcurvename'),
+  OL_HASH_FUNCTION_NAME_FQN: FQN.of('ol.hashfunctionname'),
+  OL_ID_FQN: FQN.of('ol.id'),
+  OL_NAME_FQN: FQN.of('ol.name'),
+  OL_ROLE_FQN: FQN.of('ol.role'),
+  OL_SCHEMA_FQN: FQN.of('ol.schema'),
+  OL_SIGNATURE_DATA_FQN: FQN.of('ol.signaturedata'),
+  OL_TEXT_FQN: FQN.of('ol.text'),
+  OL_TYPE_FQN: FQN.of('ol.type'),
+  OL_VERSION_FQN: FQN.of('ol.version'),
 };
 
 export {
